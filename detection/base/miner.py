de27ab39d6c3fa15bc72f5e8669fd897d3b99bd3
root <dalmannikita@gmail.com>
2024-03-24 12:19:04 +0000

    fix resync for miners

--- detection/base/miner.py
+++ detection/base/miner.py
@@ -179,7 +179,7 @@ class BaseMinerNeuron(BaseNeuron):
 
     def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
-        # bt.logging.info("resync_metagraph()")
+        bt.logging.info("resync_metagraph()")
 
         # Sync the metagraph.
         try:
