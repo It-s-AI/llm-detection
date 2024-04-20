a538e311c68c96d8aac14745bf87f3364a164a2b
sergak0 <dog.vol.serg@gmail.com>
2024-04-20 19:12:07 +0300

    add pile dataset

--- detection/validator/my_datasets.py
+++ detection/validator/my_datasets.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import bittensor as bt
+import numpy as np
 from datasets import load_dataset
 from collections.abc import Iterator
 from pylatexenc.latex2text import LatexNodes2Text
@@ -15,7 +16,6 @@ class HumanDataset(Iterator):
 
     def init_dataset(self):
         seed = random.randint(0, 1000)
-
         c4 = iter(
             load_dataset("allenai/c4", 'en',  streaming=True)['train'].shuffle(
                 seed=seed, buffer_size=1000
@@ -41,11 +41,64 @@ class HumanDataset(Iterator):
             return res
 
 
-class PromptDataset(Iterator):
-    def __init__(self, max_prompt_len=512):
+class PilePromptDataset(Iterator):
+    def __init__(self, max_prompt_len):
+        super().__init__()
+        self.pile = self.init_dataset()
+        self.max_prompt_len = max_prompt_len
+
+    def init_dataset(self):
+        seed = random.randint(0, 1000)
+        dataset = iter(
+            load_dataset("monology/pile-uncopyrighted", name="train", streaming=True)['train'].shuffle(
+                seed=seed, buffer_size=1000
+            )
+        )
+        return dataset
+
+    def generate_prompt(self, context):
+        payload_cuttoff = int(len(context) * np.random.uniform(0.1, 0.9))
+        prompt_payload = context[:payload_cuttoff]
+
+        user_request = np.random.choice([
+            f'Complete the following document.\n\n"{prompt_payload}"',
+            f'Finish writing the following document.\n\n"{prompt_payload}"',
+            f'Help me finish writing the following.\n\n"{prompt_payload}"',
+            f'Help me complete this: "{prompt_payload}"',
+            f'Finish writing the following (be careful not to stop prematurely): "{prompt_payload}"',
+        ])
+
+        leading_words = np.random.choice([
+            context[payload_cuttoff:],
+            f"Here's a plausible continuation for the document: {context[payload_cuttoff:]}",
+            f"Sure! Here's the rest of the document: \"{context[payload_cuttoff:]}"
+        ])
+
+        return f"A chat.\nUSER: {user_request}\nASSISTANT: {leading_words}"
+
+    def __next__(self):
+        while True:
+            try:
+                el = next(self.pile)
+                document_text = el['text']
+                context_len = int(min(len(document_text), self.max_prompt_len * 1.25) * np.random.uniform(0.25, 0.75))
+                prompt = self.generate_prompt(document_text[:context_len])[:self.max_prompt_len]
+                return {'prompt': prompt, 'topic': el['meta']['pile_set_name'], 'real_completion': document_text[context_len:]}
+            except Exception as e:
+                if type(e) == StopIteration:
+                    bt.logging.info('PilePromptDataset ended: reinitializing it')
+                else:
+                    bt.logging.error("Got exception during loading data from PilePromptDataset, reinitializing it")
+                    bt.logging.exception(e)
+
+                self.pile = self.init_dataset()
+                continue
+
+
+class HC3PromptDataset(Iterator):
+    def __init__(self, max_prompt_len):
         super().__init__()
         self.hc3 = self.init_dataset()
-        self.prompt_generator = PromptGenerator()
         self.max_prompt_len = max_prompt_len
 
     def init_dataset(self):
@@ -57,7 +110,7 @@ class PromptDataset(Iterator):
         )
         return hc3
 
-    def get_hc3_prompt(self):
+    def __next__(self):
         while True:
             try:
                 el = next(self.hc3)
@@ -67,7 +120,6 @@ class PromptDataset(Iterator):
                 else:
                     while el['source'] != 'reddit_eli5':
                         el = next(self.hc3)
-                el['text'] = el['text'].replace("Explain like I'm five.", '').replace("Please explain like I'm five.", '')
             except Exception as e:
                 if type(e) == StopIteration:
                     bt.logging.info('Prompt dataset ended: reinitializing it')
@@ -78,27 +130,42 @@ class PromptDataset(Iterator):
                 self.hc3 = self.init_dataset()
                 continue
 
-            el['prompt'] = el['question']
-            return el
+            el['question'] = el['question'].replace("Explain like I'm five.", '').replace("Please explain like I'm five.", '')
+            el['question'] = el['question'][:self.max_prompt_len]
+            return {'prompt': el['question'], 'topic': el['source']}
+
+
+class PromptDataset(Iterator):
+    def __init__(self, max_prompt_len=1024):
+        super().__init__()
+        self.hc3_prompt_dataset = HC3PromptDataset(max_prompt_len)
+        self.pile_prompt_dataset = PilePromptDataset(max_prompt_len)
+        self.prompt_generator = PromptGenerator()
+        self.max_prompt_len = max_prompt_len
 
     def __next__(self) -> dict:
         while True:
             # bt.logging.debug("Retrieving data from PromptDataset...")
             res = {}
-            if random.random() < 0.3:
+            p = random.random()
+            if p < 0.3:
                 bt.logging.debug("Getting prompt from hc3")
-                el = self.get_hc3_prompt()
+                el = next(self.hc3_prompt_dataset)
                 res['data_source'] = 'hc3'
-            else:
+            elif p < 0.6:
                 bt.logging.debug("Getting prompt from prompt_generator")
                 el = self.prompt_generator.get_challenge(None)
                 res['data_source'] = 'prompt_generator'
+            else:
+                bt.logging.debug("Getting prompt from pile")
+                el = next(self.pile_prompt_dataset)
+                res['data_source'] = 'pile'
 
             if len(el['prompt']) > self.max_prompt_len:
                 bt.logging.info("Prompt has len {}, truncating it to {} chars".format(len(el['prompt']), self.max_prompt_len))
 
             res['prompt'] = el["prompt"][:self.max_prompt_len]
-            res['task_name'] = el['task'] if res['data_source'] == 'prompt_generator' else el['source']
+            res['topic'] = el['task'] if res['data_source'] == 'prompt_generator' else el['topic']
 
             # Check if the text is not empty or does not consist only of newline characters
             if res['prompt'].strip():
