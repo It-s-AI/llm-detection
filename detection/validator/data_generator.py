dc4c835d1ba4b6846fee9ae918f40c7174872aa9
sergak0 <dog.vol.serg@gmail.com>
2024-11-01 13:06:15 +0300

    fix saving in data_generator.py

--- detection/validator/data_generator.py
+++ detection/validator/data_generator.py
@@ -186,7 +186,7 @@ def main(input_path, output_path, n_samples, n_ai_samples, n_human_samples):
             try:
                 start_ind = len(full_data) // 10000 * 10000
                 cur_path = output_path[:output_path.rfind('.')] + '_{}'.format(start_ind) + output_path[output_path.rfind('.'):]
-                df[start_ind:].to_pickle(cur_path, index=False)
+                df[start_ind:].to_pickle(cur_path)
                 bt.logging.info("Saved {} samples into {}".format(len(df[start_ind:]), cur_path))
             except:
                 bt.logging.error("Coudnt save data into file: {}".format(traceback.format_exc()))
